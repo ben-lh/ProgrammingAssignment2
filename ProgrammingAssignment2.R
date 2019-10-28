a6a5dd52ddd0d2b521b7b6962fa5ae047bf4048a
ben-lh <40232584+ben-lh@users.noreply.github.com>
2019-10-28 16:16:32 +0100

    Update ProgrammingAssignment2.R

--- ProgrammingAssignment2.R
+++ ProgrammingAssignment2.R
@@ -9,21 +9,23 @@
 ## whith that, you are allowed to extand & generalize the inverse notion for non squares matrices 
 ## as long as they have a value in a algebraic field
 ## if the matrix is already invertible it work as solve function 
+
 library(MASS)
 
 ## inspire from Vector example, change name and function mean to ginv
-## take a matrix as paramater and add set & get function re-use in cacheSolve
+## take a matrix as paramater and add set & get function re-use matrix value in cacheSolve
+
 makeCacheMatrix <- function(x = matrix()) {
         m <- NULL
-        set <- function(y) {
+        set <- function(y) {                 # initialize the set function,use to reset the cache value
                 x <<- y
                 m <<- NULL
         }
-        get <- function() x
-        setinv <- function(ginv) m <<- ginv
-        getinv <- function() m
-        list(set = set, get = get,
-             setinv = setinv,
+        get <- function() x                  # get take no arg and result the matrix (take in cache)
+        setinv <- function(ginv) m <<- ginv  # setinv apply the inverse function
+        getinv <- function() m               # permit to use the result of cacheSolve
+        list(set = set, get = get,           # define set() get() as name set get
+             setinv = setinv,                # setinv() getinv as name setinv getinv
              getinv = getinv)
         
 }
@@ -32,32 +34,32 @@ makeCacheMatrix <- function(x = matrix()) {
 ## inspire from Vector example, change name setmean/getmean to setinv/getinv and function mean to ginv
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-        m <- x$getinv()
-        if(!is.null(m)) {
-                message("getting cached data")
+        m <- x$getinv()                         # m take value of x and getinv function from makeCacheMatrix 
+        if(!is.null(m)) {                       # condition to see if you have already something in cache
+                message("getting cached data") 
                 return(m)
         }
-        data <- x$get()
-        m <- ginv(data, ...)
-        x$setinv(m)
-        m
+        data <- x$get()                         # data take value of x and get function from makeCacheMatrix 
+        m <- ginv(data, ...)                    # m take parameter
+        x$setinv(m)                             # from x apply setinv from makeCacheMatrix on m value
+        m                                       # return value of m
 }
 
 #example of use :
+
 x<-matrix(1:4,nrow = 2, ncol = 2)
 
-xMatrix<-makeCacheMatrix(x) #value of makeCacheMtrix in xMatrix
-xMatrix$get() #get the value of x
+xMatrix<-makeCacheMatrix(x)             # makeCacheMtrix(x) in the variable xMatrix
+xMatrix$get()                           # get the value of x
 
 a<-c(1,2,3,4)
 b<-c(2,4,9,12)
 x<-matrix(c(a,b),nrow = 4, ncol = 2)
 
-xMatrix$set(x) #reset cache value with an other value of x
-xMatrix$get() #get the value of x
-cacheSolve(xMatrix) #apply calcul on the x and cache it
-inv <- xMatrix$getinv() #return the value in cache without using cacheSolve fucntion
+xMatrix$set(x)                           # reset cache value with an other value of x
+xMatrix$get()                            # get the value of x
+cacheSolve(xMatrix)                      # apply calcul on the x and cache it
+inv <- xMatrix$getinv()                  # return the value in cache without using cacheSolve fucntion
 
 inv
 
